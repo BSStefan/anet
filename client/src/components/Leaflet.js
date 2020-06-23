39de8cf9bd49adb6baa389159ebb48e13b8c87bb
Yusuf Coşkun <yusuf.coskun@simsoft.com.tr>
2020-06-23 13:25:07 +0300

    nci-agency/anet#3082: Display report conflicts in map

--- client/src/components/Leaflet.js
+++ client/src/components/Leaflet.js
@@ -133,6 +133,9 @@ const Leaflet = ({
         if (m.onMove) {
           marker.on("moveend", event => m.onMove(event, map))
         }
+        if (m.popupOpen) {
+          marker.on("popupopen", event => m.popupOpen(event))
+        }
         markerLayer.addLayer(marker)
       })
 
